061d950a5f3715edf21ba9b2a406305a0b05b1a4
Guilherme D. Sousa <guifgd36@gmail.com>
2024-03-20 22:47:27 -0300

    Initial commit

---
+++ Principal.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import view.telaController;
+
+public class Principal extends Application{
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(Principal.class.getResource("view/tela.fxml"));
+        Parent root = loader.load();
+        telaController tela = new telaController();
+        loader.setController(tela);
+
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("Sistema Shinobi de Controle de Missões");
+        stage.show();
+    }
+    public static void main(String[] args) {
+        System.out.println ("Hola Mundo!");
+        launch(args);
+    }
+}
